6c99a1b00742a77f7f174119feef3d031e661e0c
kr4xkan <kr4xkan@gmail.com>
2022-09-22 16:41:54 +0200

    prep matrix

--- neural-net/neural-net.c
+++ neural-net/neural-net.c
@@ -1,10 +1,37 @@
 #include "err.h"
 #include <SDL2/SDL_image.h>
+#include <stdlib.h>
 
 #include "../utils.h"
 #include "neural-net.h"
 
 SDL_Surface *surface;
+
+int main(int argc, char **argv) {
+    if (argc < 4 || argc > 8) {
+        errx(1, "\nUsage:\n"
+                "./neural nb_input nb_hidden1 ... nb_hidden(n) nb_output\n\n"
+                "Maximum of 5 hidden layers");
+    }
+
+    int layer_count = argc - 1;
+    int layers_node_count[6];
+
+    char *ptr;
+    for (int i = 1; i < argc; i++) {
+        layers_node_count[i - 1] = strtol(argv[i], &ptr, 10);
+    }
+
+    // Build neural network
+    float *weights_ih =
+        malloc(layers_node_count[0] * layers_node_count[1] * sizeof(float));
+    
+    float *weights_ho =
+        malloc(layers_node_count[1] * layers_node_count[2] * sizeof(float));
+
+    free(weights_ih);
+    free(weights_ho);
+}
 
 void neural_test() {
     surface = IMG_Load("DataSample/neuralnet/image.bmp");

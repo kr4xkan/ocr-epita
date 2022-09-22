19e6abfeee7c41877cbdddb5448f5d10ea19e6a4
romain.doulaud <romain.doulaud@epita.fr>
2022-09-22 18:09:47 +0100

    j'ai deux courbes let's goooo

--- cutter/cutter.c
+++ cutter/cutter.c
@@ -11,7 +11,7 @@ int maxDist;
 void DetectLines() {
     //Load the image
     SDL_Surface *surface;
-    surface = IMG_Load("DataSample/cutter/minisquare.bmp");
+    surface = IMG_Load("DataSample/cutter/2pixels.bmp");
     if (!surface) {
         errx(1, "Could not load image");
     }
@@ -21,9 +21,11 @@ void DetectLines() {
     printf("Image Loaded Succesfully with dimension: %dx%d!\n", w, h);
 
 
-    // define the parameter spaces 
+    // Creating the parameter space
+    // x: 180 (-pi/2 to pi/2)  y: -max_dist to max_dist 
+    // maxDist is the length of the diagonal of the image
     maxDist = (int) sqrt((double)w*w + h*h) + 1;
-    printf("%i\n", maxDist * 180);
+    printf("maxDist: %i\n", maxDist);
 
     // Allocating a big chunk of memory during the compilation
     unsigned char * space = calloc(maxDist * 360, sizeof(unsigned char));
@@ -39,14 +41,14 @@ void DetectLines() {
 
             GetPixelColor(surface, x, y, &r, &g, &b);
             unsigned char average = (r + g + b) / 3;
-            if (average > 0){
+            if (average > 200){
+                printf("found one at: (%i, %i)!  Average: %u\n", x, y, average);
                 FillAcumulator(x, y, space);
             }
         }
-        printf("%i  ", y);
     }
-    
-    for(int i = 0; i < maxDist * 180; i ++){
+
+    for(int i = 0; i < maxDist * 360; i ++){
         if(i % 180 == 0)
             printf("\n");
         if(space[i] != 0)
@@ -66,13 +68,21 @@ void DetectLines() {
 
 
 
-void FillAcumulator(int x, int y, unsigned char * space){
+void FillAcumulator(int a, int b, unsigned char * space){
     //printf("%i %i\n", x, y);
+
+    int x = a+1;
+    int y = b+1;
     for (int teta = -90; teta <= 90; teta += 1){
         float angle = teta * 3.141592 / 180;
 //        printf("%f\n", angle);
         int rho = x*cos(angle) + y*sin(angle) + maxDist;
   //      printf("%i, %i, %i \n", teta, rho, rho*180 + teta + 90);
+
+        if(teta == -90 || teta == -45 || teta == 0 || teta == 45 || teta == 90){
+            printf("for teta = %i, rho = %i\n", teta, rho-maxDist);
+
+        }
         space[rho*180 + teta + 90] += 1;
     }
 }

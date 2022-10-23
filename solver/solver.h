97f9d59740daa985a564098fb7f2ad527a4ada92
Camille Chapelle <camille.chapelle@epita.fr>
2022-10-23 20:10:26 +0200

    solver : print resolved grid

--- solver/solver.h
+++ solver/solver.h
@@ -1,1 +1,7 @@
 #pragma once
+
+
+void print(int grid[10][10]);
+int check_safe(int grid[10][10], int row, int col, int num);
+int solver(int grid[10][10], int row, int col);
+void argv_to_grid(char *filename, int grid[10][10]);

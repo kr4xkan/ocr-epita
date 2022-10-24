c3fe91c43463ce481bb4855aad2d50d90fda0372
kr4xkan <kr4xkan@gmail.com>
2022-10-24 17:06:35 +0200

    increase otsu type limit

--- preprocessing/preprocessing.c
+++ preprocessing/preprocessing.c
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <err.h>
 
 SDL_Surface* load_image(const char* path)
 {
@@ -34,7 +35,7 @@ void surface_to_grayscale(SDL_Surface* surface)
 int otsu(SDL_Surface* img, int w, int h)
 {
     Uint32 *pixels = img->pixels;
-    float histo[256];
+    double histo[256];
     int nbpix = w * h;
     int threshold = 0;
     for (int x = 0; x < w; x++)
@@ -48,28 +49,28 @@ int otsu(SDL_Surface* img, int w, int h)
         }
     }
 
-    float w1 = 0; //sum of all expectations
+    double w1 = 0; //sum of all expectations
     for (int i = 0; i <= 255; i++)
     {
         w1 += i * ((int)histo[i]);
     }
 
-    float w2 = 0; //expectation sum 2
-    int n1 = 0; //histo value for i
-    int n2; //histo value for all others
-    float m1; //mean value 1
-    float m2; //mean value 2
-    float var; //each value var to compare with maxvar
-    float maxvar = 0; //max variance : result
+    double w2 = 0; //expectation sum 2
+    long n1 = 0; //histo value for i
+    long n2; //histo value for all others
+    double m1; //mean value 1
+    double m2; //mean value 2
+    double var; //each value var to compare with maxvar
+    double maxvar = 0; //max variance : result
 
     for (int i = 0 ; i <= 255 ; i++) //calcul of the best threshold : the one who as the greatest variance.
     {
         n1 += histo[i];
         n2 = nbpix - n1;
-        w2 += (float) (i * ((int)histo[i]));
+        w2 += (double) (i * ((int)histo[i]));
         m1 = w2 / n1; //mean 1
         m2 = (w1 - w2) / n2; //mean 2
-        var = (float) n1 * (float) n2 * (m1 - m2) * (m1 - m2);
+        var = (double) n1 * (double) n2 * (m1 - m2) * (m1 - m2);
         if (var > maxvar)
         {
             maxvar = var;
@@ -114,4 +115,14 @@ void binarization(char *path){
     IMG_SavePNG(surf2, "dumb.png");
 
     SDL_FreeSurface(surface);
+
+    SDL_FreeSurface(surf2);
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        errx(1, "Usage: ./preprocessing image_path");
+    }
+
+    binarization(argv[1]);
 }

49f85d888af7e3a8218b5a80d15ddaa5fb7b1a19
Camille Chapelle <camille.chapelle@epita.fr>
2022-12-07 16:53:55 +0100

    bonjour

--- ui/ui.c
+++ ui/ui.c
@@ -7,14 +7,12 @@ GtkWindow *window2;
 GtkWidget *fixed_container_1;
 GtkPaned *paned;
 
-GtkButton *button1;
 GtkButton *open_button;
 
 GtkLabel *label1;
 GtkBuilder *builder;
-GtkEntry *text_entry;
+GtkFileChooserButton *file_chooser;
 
-GtkDrawingArea *area;
 
 
 //configure-event
@@ -32,36 +30,10 @@ gboolean on_configure_opening(GtkWidget *widget, GdkEvent *event, gpointer user_
 	return FALSE;
 }
 
-//draws the image
-gboolean on_draw(GtkWidget *widget, cairo_t *cr, gpointer user_data){
-	cairo_set_source_rgb(cr,1,1,1);
-	cairo_paint(cr);
-
-}
-
-
-//gets the filepath and hide open window
-const gchar* get_entry(){
-	return gtk_entry_get_text(text_entry);
+gchar* select_file(GtkWindow *window, GdkEvent *event, gpointer file_chooser){
+	return gtk_file_chooser_get_uri(GTK_FILE_CHOOSER(file_chooser));
 }
 	
-
-
-gboolean on_open_button(GtkWidget *widget, cairo_t *cr, gpointer user_data){
-	gchar *path = malloc(128 * sizeof(char));
-	strcpy(path, get_entry());
-	FILE *im = fopen(path, "r");
-	if (!im){
-		gtk_label_set_text(GTK_LABEL(label1),(const gchar*) "Could Not Load Image");
-		return FALSE;
-	}
-	gtk_widget_hide(GTK_WIDGET(opening_window));
-	gtk_widget_show(GTK_WIDGET(window2));
-	user_data = im;
-	return TRUE;
-}
-
-
 
 int main(){
 	
@@ -80,19 +52,19 @@ int main(){
 	opening_window = GTK_WINDOW(gtk_builder_get_object(builder, "opening_window"));
 	window2 = GTK_WINDOW(gtk_builder_get_object(builder, "window2"));
 	fixed_container_1 = GTK_WIDGET(gtk_builder_get_object(builder, "fixed_container_1"));
-	button1 = GTK_BUTTON(gtk_builder_get_object(builder, "button1"));
 	label1 = GTK_LABEL(gtk_builder_get_object(builder, "label1"));
-	text_entry = GTK_ENTRY(gtk_builder_get_object(builder, "text_entry"));
+	file_chooser = GTK_FILE_CHOOSER_BUTTON(gtk_builder_get_object(builder, "file_chooser"));
 	open_button =  GTK_BUTTON(gtk_builder_get_object(builder, "open_button"));
 	paned = GTK_PANED(gtk_builder_get_object(builder, "paned"));
 	area = GTK_DRAWING_AREA(gtk_builder_get_object(builder, "area"));
-	FILE* im;
+
+
 	//signals
 	g_signal_connect(opening_window, "destroy", G_CALLBACK(gtk_main_quit),NULL);
 	g_signal_connect(window2, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 	g_signal_connect(opening_window, "configure-event", G_CALLBACK(on_configure_opening), &fixed_container_1);
-	g_signal_connect(open_button, "clicked", G_CALLBACK(on_open_button), &im);
-	g_signal_connect(area, "draw", G_CALLBACK(on_draw), &area);
+	//g_signal_connect(open_button, "clicked", G_CALLBACK(on_open_button), &);
+	g_signal_connect(file_chooser, "file-set", G_CALLBACK(select_file), &file_chooser);
 
 	//show windows
 	gtk_widget_hide(GTK_WIDGET(window2));

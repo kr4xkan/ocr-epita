305ae971065355bd0654cb579d7d6ed749f03594
Hugo Baptista <hugo.baptista@epita.fr>
2022-12-11 11:00:37 +0100

    dataset enhancer

---
+++ DataSample/number-generator/enhance.py
@@ -0,0 +1,64 @@
+from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageOps, ImageChops
+import shutil
+import os
+import random
+import hashlib
+import base64
+import sys
+
+
+def enhance(R):
+    files = os.listdir("./output/")
+    l = R * len(files)
+    i = 1
+    for r in range(R):
+        for f in files:
+            print(i,"/",l)
+            randrot = random.randrange(-10, 10)
+            randoff = random.randrange(-2, 2)
+            og = Image.open("./output/" + f)
+            mod = og.copy()
+            mod = mod.rotate(randrot, fillcolor=0)
+            mod = ImageChops.offset(mod, randoff)
+            filename = hashlib.sha256(str(random.getrandbits(256)).encode('utf-8')).hexdigest()[:8]
+            fname = f[0]+"__"+filename+".bmp"
+            mod.save("./noise/" + fname, format="BMP")
+            i += 1
+
+def to_dir(path):
+    files = os.listdir(path)
+    labels = open(path+"labels.nrl")
+    mapper = []
+    for l in labels:
+        mapper.append(l.strip())
+    print(mapper)
+    for f in files:
+        if f != "labels.nrl":
+            y = int(f[0]) - 1
+            x = int(f[2]) - 1 
+            filename = hashlib.sha256(str(random.getrandbits(256)).encode('utf-8')).hexdigest()[:8]
+            final = "./output/" + mapper[y * 9 + x] + "__" + filename + ".png"
+            shutil.copyfile(path+f, final)
+
+def equalize():
+    files = os.listdir("./output")
+    histo = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for f in files:
+        idx = int(f[0])
+        histo[idx] += 1
+    print(histo)
+   # lower = min(histo)
+   # to_delete = [ x - lower for x in histo ]
+   # print(to_delete)
+   # for f in files:
+   #     idx = int(f[0])
+   #     if to_delete[idx] != 0:
+   #         os.remove("./output/"+f)
+   #         to_delete[idx] -= 1
+
+if __name__ == "__main__":
+    # equalize()
+    # to_dir(sys.argv[1])
+    # shutil.rmtree("./noise")
+    # os.mkdir("./noise")
+    enhance(int(sys.argv[1]))
